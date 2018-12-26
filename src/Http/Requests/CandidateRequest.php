35a4d2bf6c2567a08e55314e766c247ec69a260c
Thiago Brauer Ribeiro <thiagobrauer@gmail.com>
2018-12-26 17:42:47 -0300

    update

--- src/Http/Requests/CandidateRequest.php
+++ src/Http/Requests/CandidateRequest.php
@@ -9,12 +9,15 @@ class CandidateRequest extends IORequest
   public function sanitize(){
     $input = parent::sanitize();
 
-    $input['birthday'] = $input['birthday_submit'];
+    if(isset($input['birthday_submit']))
+      $input['birthday'] = $input['birthday_submit'];
     $input['cpf'] = str_replace(".", "", $input['cpf']);
     $input['cpf'] = str_replace("-", "", $input['cpf']);
     
-    $input['address_city'] = $input['__city'];
-    $input['address_state'] = $input['__state'];
+    if(isset($input['__city']))
+      $input['address_city'] = $input['__city'];
+    if(isset($input['__state']))
+      $input['address_state'] = $input['__state'];
 
     $this->replace($input);
 	}
