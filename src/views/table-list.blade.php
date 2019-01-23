f7760fbf4e1a7ea871e8a283229bd011790d544f
Thiago Brauer Ribeiro <thiagobrauer@gmail.com>
2019-01-23 17:35:15 -0300

    Update table-list.blade

--- src/views/table-list.blade.php
+++ src/views/table-list.blade.php
@@ -1,20 +1,38 @@
-	<div class = 'row dt-filters-container'>
-		<div class="col-12">
-			<div class="form-group">
-        <label for = 'subtitulo' class = 'bmd-label-static'><i class = 'ico ico-filter'></i> Palavra Chave</label>
-        <input type = 'text' class = 'form-control form-control-lg' name ='ft_search' id = 'ft_search' />
-			</div>
+<div class = 'row dt-filters-container'>
+	<div class="col-12">
+		<div class="form-group">
+	<label for = 'subtitulo' class = 'bmd-label-static'><i class = 'ico ico-filter'></i> Palavra Chave</label>
+	<input type = 'text' class = 'form-control form-control-lg' name ='ft_search' id = 'ft_search' />
 		</div>
-  </div>
+	</div>
+</div>
+  @php
+	$pkg = json_decode(file_get_contents(base_path('composer.json')),true);
+	$hasSpatie = array_has($pkg, 'require.spatie/laravel-permission');  
+@endphp
+@if ($hasSpatie) 
 	@component('IntranetOne::io.components.datatable',[
-	"_id" => "default-table",
-	"_columns"=> [
-			["title" => "CNPJ"],
-			["title" => "Razão Social"],
-			["title" => "Nome Fantasia"],
-			["title" => "Ativo?"],
-			["title" => "Recrutador?"],
-			["title" => "Ações"],
-		]
-	])
-@endcomponent
+		"_id" => "default-table",
+		"_columns"=> [
+				["title" => "CNPJ"],
+				["title" => "Razão Social"],
+				["title" => "Nome Fantasia"],
+				["title" => "Ativo?"],
+				["title" => "Recrutador?"],
+				["title" => "Ações"],
+			]
+		])
+	@endcomponent	
+@else 
+	@component('IntranetOne::io.components.datatable',[
+		"_id" => "default-table",
+		"_columns"=> [
+				["title" => "CNPJ"],
+				["title" => "Razão Social"],
+				["title" => "Nome Fantasia"],
+				["title" => "Ativo?"],
+				["title" => "Ações"],
+			]
+		])
+	@endcomponent	
+@endif
